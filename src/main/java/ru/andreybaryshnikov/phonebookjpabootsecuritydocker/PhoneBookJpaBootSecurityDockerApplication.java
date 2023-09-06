226ee25374f1cafe155c80bae9b606add6254d5a
Андрей Барышников <andrey1andrey@narod.ru>
2023-09-06 22:07:24 +0300

    Initial commit

---
+++ src/main/java/ru/andreybaryshnikov/phonebookjpabootsecuritydocker/PhoneBookJpaBootSecurityDockerApplication.java
@@ -0,0 +1,13 @@
+package ru.andreybaryshnikov.phonebookjpabootsecuritydocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneBookJpaBootSecurityDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneBookJpaBootSecurityDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/andreybaryshnikov/phonebookjpabootsecuritydocker/PhoneBookJpaBootSecurityDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.andreybaryshnikov.phonebookjpabootsecuritydocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneBookJpaBootSecurityDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
